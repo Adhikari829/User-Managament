4cf7ba0d8a808e8f6ddd5697911973e5929b7419
Adhikari829 <adhikarikanchan@153gmail.com>
2024-10-13 10:33:02 +0530

    Some changes

--- src/main/java/org/jp/entity/UserRole.java
+++ src/main/java/org/jp/entity/UserRole.java
@@ -1,17 +1,17 @@
 package org.jp.entity;
 
-import java.util.List;
 
-import jakarta.persistence.CascadeType;
+
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+
 import jakarta.persistence.Table;
 
-import jakarta.persistence.*;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
